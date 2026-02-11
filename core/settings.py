15f4845d7cd74521999d3cf18baf99de86a144ef
Othnieladventurer <hefreakbeatz@gmail.com>
2026-02-11 18:17:30 -0500

    Ready for production2

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,9 @@ import os
 from pathlib import Path
 os.environ['TZ'] = 'America/New_York'
 import environ
-import dj_database_url
+from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
